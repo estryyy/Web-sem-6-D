6752586c7af8ef8e8f33c57dcfb38ab2db5f9569
estryyy <ningsihestri@gmail.com>
2022-05-02 20:28:04 +0800

    Pemecahan starter.html

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "partials/head.php" ?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body class="hold-transition sidebar-mini">
+  <div class="wrapper">
+    <?php include "partials/nav.php" ?>
+    <?php include "partials/sidebar.php" ?>
+    <div class="content-wrapper">
+      <?php include "partials/content.php" ?>
+    </div>
+      <?php include "partials/control.php" ?>
+      <?php include "partials/footer.php" ?>
+  </div>
+</body>
+</html>
