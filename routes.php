2ff6974a3ca6e0826ab2c4133fde39fef3e9ed0e
estryyy <ningsihestri@gmail.com>
2022-05-02 22:30:20 +0800

    Membuat Halaman Tambah Data

--- routes.php
+++ routes.php
@@ -6,11 +6,14 @@
             case 'home';
                 file_exists('pages/home.php') ? include 'pages/home.php' : include 'pages/404.php';
                 break;
-            default:
-                include 'pages/404.php';
             case 'lokasiread';
                 file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include 'pages/404.php';
                 break;
+            case 'lokasicreate';
+                file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include 'pages/404.php';
+                break;
+            default:
+                include 'pages/404.php';
         }
     }else{
         include 'pages/home.php';
