3373b861b5c574e5dd9272de476fb7630fd77c85
estryyy <ningsihestri@gmail.com>
2022-05-02 21:57:18 +0800

    Menampilkan halaman read lokasi

--- routes.php
+++ routes.php
@@ -8,6 +8,9 @@
                 break;
             default:
                 include 'pages/404.php';
+            case 'lokasiread';
+                file_exists('pages/admin/lokasiread.php') ? include 'pages/admin/lokasiread.php' : include 'pages/404.php';
+                break;
         }
     }else{
         include 'pages/home.php';
